73f4446d58327380c9c16b2f81701cb22b0db4cd
Dongdong Zhou <dz@yay.com>
2022-02-18 21:06:52 +0000

    update rust-analyzer

--- src/main.rs
+++ src/main.rs
@@ -52,7 +52,7 @@ impl LapcePlugin for State {
         );
         if !PathBuf::from(&file_name).exists() {
             let url = format!(
-                "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-02-07/{}.gz",
+                "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-02-14/{}.gz",
                 file_name
             );
             let gz_file = PathBuf::from(file_name.clone() + ".gz");
