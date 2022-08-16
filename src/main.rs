9c9ca14549e2636b46c12b6dca34c8a66c31dd85
Dongdong Zhou <dz@yay.com>
2022-08-16 21:31:48 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 use std::{
+    error::Error,
     fs::File,
     io::{self, Read, Write},
     path::PathBuf,
-    process::Command,
 };
 
+use anyhow::Result;
 use flate2::read::GzDecoder;
-use lapce_plugin::{register_plugin, LapcePlugin, PLUGIN_RPC};
+use lapce_plugin::{register_plugin, Http, LapcePlugin, PLUGIN_RPC};
 use lsp_types::{
     request::{Initialize, Request},
     InitializeParams,
@@ -32,16 +33,47 @@ pub struct Configuration {
 
 register_plugin!(State);
 
+fn initialize(params: InitializeParams) -> Result<()> {
+    let arch = match std::env::var("ARCH").as_deref() {
+        Ok("x86_64") => "x86_64",
+        Ok("aarch64") => "aarch64",
+        _ => return Ok(()),
+    };
+    let os = match std::env::var("OS").as_deref() {
+        Ok("linux") => "unknown-linux-gnu",
+        Ok("macos") => "apple-darwin",
+        Ok("windows") => "pc-windows-msvc",
+        _ => return Ok(()),
+    };
+    let file_name = format!("rust-analyzer-{}-{}", arch, os);
+    let file_path = PathBuf::from(&file_name);
+    let gz_path = PathBuf::from(file_name.clone() + ".gz");
+    if !file_path.exists() {
+        let url = format!(
+            "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-07-18/{}.gz",
+            file_name
+        );
+        PLUGIN_RPC.stderr(&format!("url {url}"));
+        let mut resp = Http::get(&url)?;
+        PLUGIN_RPC.stderr(&format!("status code {}", resp.status_code));
+        let body = resp.body_read_all()?;
+        std::fs::write(&gz_path, body)?;
+        let mut gz = GzDecoder::new(File::open(&gz_path)?);
+        let mut file = File::create(&file_path)?;
+        std::io::copy(&mut gz, &mut file)?;
+        std::fs::remove_file(&gz_path)?;
+    }
+
+    PLUGIN_RPC.start_lsp(file_path, "rust", params.initialization_options);
+    Ok(())
+}
+
 impl LapcePlugin for State {
     fn handle_request(&mut self, id: u64, method: String, params: Value) {
         match method.as_str() {
             Initialize::METHOD => {
                 let params: InitializeParams = serde_json::from_value(params).unwrap();
-                PLUGIN_RPC.start_lsp(
-                    PathBuf::from("rust-analyzer-aarch64-apple-darwin"),
-                    "rust",
-                    params.initialization_options,
-                );
+                let _ = initialize(params);
             }
             _ => {}
         }
