3bf7cfbc20c70647544e19738d9c953ff76e90ce
Dongdong Zhou <dz@yay.com>
2022-08-31 09:48:21 +0100

    bump ra version

--- src/main.rs
+++ src/main.rs
@@ -61,7 +61,7 @@ fn initialize(params: InitializeParams) -> Result<()> {
     let gz_path = PathBuf::from(file_name.clone() + ".gz");
     if !file_path.exists() {
         let url = format!(
-            "https://github.com/rust-analyzer/rust-analyzer/releases/download/2022-07-18/{}.gz",
+            "https://github.com/rust-lang/rust-analyzer/releases/download/2022-08-29/{}.gz",
             file_name
         );
         let mut resp = Http::get(&url)?;
